1e96e4907eebad699571014f518c84e09ebc5d57
geraosio <geraosio@gmail.com>
2020-07-22 19:15:47 -0500

    Add Semaphore component

--- src/components/Header/Navbar.vue
+++ src/components/Header/Navbar.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator'
 
 @Component
 export default class Navbar extends Vue {}
